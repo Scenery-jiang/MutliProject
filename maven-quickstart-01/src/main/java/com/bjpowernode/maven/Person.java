5b11cf38e49c23b4a4210f2ac4cb41d8998cfb79
jiangjing <1069451454@qq.com>
2020-02-06 09:23:08 +0800

    初始化提交person 的name属性

---
+++ maven-quickstart-01/src/main/java/com/bjpowernode/maven/Person.java
@@ -0,0 +1,14 @@
+package com.bjpowernode.maven;
+
+/**
+ * ClassName:Person
+ * Package:com.bjpowernode.maven
+ * Description:
+ *
+ * @date:2020/2/6 9:22
+ * @author:jiangjing
+ */
+
+public class Person {
+    private String name;
+}
