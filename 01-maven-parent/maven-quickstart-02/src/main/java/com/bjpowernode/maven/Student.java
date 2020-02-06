814bfec2dcbb2db5e9c10e903ebdd87464f669a7
jiangjing <1069451454@qq.com>
2020-02-06 09:10:56 +0800

    初始化提交Student

---
+++ 01-maven-parent/maven-quickstart-02/src/main/java/com/bjpowernode/maven/Student.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.maven;
+
+/**
+ * ClassName:Student
+ * Package:com.bjpowernode.maven
+ * Description:
+ *
+ * @date:2020/2/6 9:08
+ * @author:jiangjing
+ */
+
+public class Student {
+    private String name;
+
+}
